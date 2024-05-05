dded8810891701b1bfcd0320c19a5693a1a6cbb4
kio123eee <89443091+kio123eee@users.noreply.github.com>
2024-05-06 04:40:22 +0800

    Update db_config.php

--- db_config.php
+++ db_config.php
@@ -1,8 +1,8 @@
 <?php
 // Database connection parameters
-   $db_name = 'mysql:host=viaduct.proxy.rlwy.net;port=15223;dbname=railway';
+   $db_name = 'mysql:host=roundhouse.proxy.rlwy.net;port=42743;dbname=railway';
    $user_name = 'root'; // Replace 'your_mysql_username' with your actual MySQL username
-   $user_password = 'TSyEAqhggHUXqkiRPjuDYvHEhNeRyDUu'; // Use your MySQL password here
+   $user_password = 'MkGgJpvCUwuZuNPOvonZSmHDlBUKgcMS'; // Use your MySQL password here
 try {
     // Create a PDO connection to MySQL
     $conn = new PDO($db_name, $user_name, $user_password);
