dee7b47a6d95657ddfe786d365ba54e78bc58e7f
kio123eee <89443091+kio123eee@users.noreply.github.com>
2024-05-06 02:23:12 +0800

    Update db.php

--- db.php
+++ db.php
@@ -1,8 +1,8 @@
 <?php
-$servername = "localhost";
-$username = "your_username";
-$password = "your_password";
-$dbname = "your_database_name";
+$servername = "roundhouse.proxy.rlwy.net";
+$username = "root";
+$password = "TSyEAqhggHUXqkiRPjuDYvHEhNeRyDUu";
+$dbname = "railway";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
