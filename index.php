35cd2e05dd37f7cf82f09eae73958e5e1c918c55
kio123eee <89443091+kio123eee@users.noreply.github.com>
2024-05-06 03:07:35 +0800

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Student Information System</title>
+    <title>Mapua Makati Lost and Found Information System</title>
 </head>
 <body>
     <h1>Student Information System</h1>
