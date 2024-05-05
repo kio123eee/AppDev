55988bed600aa3b884dc57bf5ce5c703c99a5943
kio123eee <89443091+kio123eee@users.noreply.github.com>
2024-05-06 04:25:52 +0800

    Update db_config.php

--- db_config.php
+++ db_config.php
@@ -1,14 +1,11 @@
 <?php
 // Database connection parameters
-$db_host = 'viaduct.proxy.rlwy.net'; // Hostname or IP address of your MySQL server
-$db_port = '15223'; // Port number for MySQL (default is 3306)
-$db_name = 'railway'; // Name of your MySQL database
-$user_name = 'root'; // Replace 'root' with your actual MySQL username
-$user_password = 'TSyEAqhggHUXqkiRPjuDYvHEhNeRyDUu'; // Use your MySQL password here
-
+   $db_name = 'mysql:host=viaduct.proxy.rlwy.net;port=45564;dbname=railway';
+   $user_name = 'root'; // Replace 'your_mysql_username' with your actual MySQL username
+   $user_password = 'fEg4GAdG4-AHBb-4EhCceCG-b345cG6C'; // Use your MySQL password here
 try {
     // Create a PDO connection to MySQL
-    $conn = new PDO("mysql:host=$db_host;port=$db_port;dbname=$db_name", $user_name, $user_password);
+    $conn = new PDO($db_name, $user_name, $user_password);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     $connection_status = "Database connection successful!";
 } catch (PDOException $e) {
