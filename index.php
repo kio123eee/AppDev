750f8be837426b2572e41bbaa08213b6ac0474dc
kio123eee <89443091+kio123eee@users.noreply.github.com>
2024-05-06 04:34:20 +0800

    Update index.php

--- index.php
+++ index.php
@@ -6,8 +6,19 @@
     <title>Test Database Connection</title>
 </head>
 <body>
-    <h1>Test Database Connection</h1>
-    <?php include 'db_config.php'; ?>
-    <p><?php echo $connection_status; ?></p>
+<?php
+// Database connection parameters
+   $db_name = 'mysql:host=viaduct.proxy.rlwy.net;port=15223;dbname=railway';
+   $user_name = 'root'; // Replace 'your_mysql_username' with your actual MySQL username
+   $user_password = 'TSyEAqhggHUXqkiRPjuDYvHEhNeRyDUu'; // Use your MySQL password here
+try {
+    // Create a PDO connection to MySQL
+    $conn = new PDO($db_name, $user_name, $user_password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $connection_status = "Database connection successful!";
+} catch (PDOException $e) {
+    $connection_status = "Connection failed: " . $e->getMessage();
+}
+?>
 </body>
 </html>
