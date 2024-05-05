43a78f8560d7f76ac04d112bb1a5ef9ae176a6b4
kio123eee <89443091+kio123eee@users.noreply.github.com>
2024-05-06 07:03:46 +0800

    Update process_form.php

--- process_form.php
+++ process_form.php
@@ -1,6 +1,6 @@
 <?php
 // Include the database connection file
-include 'db_config.php';
+include 'components/connect.php';
 
 // Check if the form was submitted
 if(isset($_POST['full_name']) && isset($_POST['student_number']) && isset($_POST['contact_number']) && isset($_POST['date'])){
