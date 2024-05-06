1160f77e0431e499fc6beacd987428976004a450
kio123eee <89443091+kio123eee@users.noreply.github.com>
2024-05-07 01:10:18 +0800

    Update notify_process.php

--- notify_process.php
+++ notify_process.php
@@ -1,18 +1,7 @@
-<?php
-require __DIR__ . '/vendor/autoload.php'; // Include the Twilio PHP SDK autoload file
-
-// Twilio credentials
-$account_sid = 'ACa65a4fc4e39bb7377d4cb0e25bf6c6ea'; // Your Twilio Account SID
-$auth_token = '0163b291b5299da5b372ba748f18a80c'; // Your Twilio Auth Token
-$twilio_number = '+18288454547'; // Your Twilio phone number
-
-// Create a new Twilio client
-$client = new Twilio\Rest\Client($account_sid, $auth_token);
-
 if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['notify_item'])) {
     // Get item ID and contact number from the form submission
     $item_id = $_POST['item_id'];
-    $contact_number = $_POST['contact_number'];
+    $contact_number = '+' . $_POST['contact_number']; // Add "+" sign to the contact number
 
     // Build the message
     $message = "Hello, this is Mapua Makati Lost and Found. Your item has been found. You can retrieve it now.";
